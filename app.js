08ee163c9c5b326b3deceed7b52841c80f84c149
porteria <porteria@porterias-MacBook-Pro.local>
2022-11-01 04:31:50 -0300

    login and register draft

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
 app.use(express.static('public'));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor funcionando');
+app.listen(PORT, ()=>{
+    console.log('Servidor funcionando en Puerto '+PORT);
 });
 
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
