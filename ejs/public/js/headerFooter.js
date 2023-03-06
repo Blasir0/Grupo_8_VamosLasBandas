376df24db900f47eade0fc16a363327d47e67196
Eze Macia <ezequiel_macia@hotmail.com>
2023-03-05 23:00:02 -0300

    BurgerMenu

--- ejs/public/js/headerFooter.js
+++ ejs/public/js/headerFooter.js
@@ -1,9 +1,16 @@
-window.addEventListener('load', function(){
-    let burguerButton = document.querySelector('#burgerButton')
-    let navBar = document.querySelector('#navBar')
+let burgerButton = document.querySelector('.burger-container')
+let navBar = document.querySelector('.main-navbar')
+let ulNavBar = document.querySelector('.ulNavBar')
+let ulLiNavBar = document.querySelectorAll('.ulNavBar li')
 
-    burguerButton.addEventListener('click', function(event){
-        event.preventDefault()
-        navBar.style.display = 'block'
-    })
+burgerButton.addEventListener('click', function(event){
+    event.preventDefault()
+    navBar.style.display = 'block'
+    navBar.style.position = "absolute"
+    navBar.style.marginTop = "130px"
+    navBar.style.paddingTop = "10px"
+    navBar.style.paddingBottom = "10px"
+    navBar.style.backgroundColor = "rgb(60, 60, 60)"
+    ulNavBar.style.marginLeft = "10px"
+    ulLiNavBar.style.margin = "15px"
 })
